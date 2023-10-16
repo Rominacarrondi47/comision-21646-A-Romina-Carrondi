4430f6789ac5845fd98260ef5c5d2b8eefd4f9a1
Rominacarrondi47 <carrondiromina@gmail.com>
2023-10-16 15:14:27 -0300

    trabajando en la parte de front end

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -1,11 +1,19 @@
 import {Router} from "express";
-
+import { ctrlCreatePost, ctrlDeletePost, ctrlGetPosts, ctrlUpdatePost, ctrlView } from "../controllers/post.controllers.js";
+import { createPost, editPost } from "../models/schemas/post.schema.js";
+import { validator } from "../middlewares/validator.js";
 
 
 const postRouter = Router()
-postRouter.get("/api/tasks")
-postRouter.post("/api/post")
-postRouter.put("/api/posts/:id")
-postRouter.delete("/api/posts/:id")
+//rutas para la vista
+postRouter.get("/api/posts", ctrlView )
+//endpoint para traer todas los posteos
+postRouter.get("/api/posts", ctrlGetPosts)
+//endpoint para crear un posteo
+postRouter.post("/api/posts/:id", createPost,validator,ctrlCreatePost)
+//endpoint para modificar un posteo
+postRouter.put("/api/posts/:id", editPost,validator,ctrlUpdatePost)
+//endpoint para eliminar un posteo
+postRouter.delete("/api/posts/:id", ctrlDeletePost)
 
 export {postRouter};
